4abd682b4aa9a6e752e8304ca6acea8aebf31033
SAFA GÜNAY <safagunay@outlook.com>
2018-12-12 21:11:49 +0300

    apiKey added

--- LockerApi/Global.asax.cs
+++ LockerApi/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using LockerApi.MessageHandlers;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -15,6 +12,7 @@ namespace LockerApi
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new APIKeyMessageHandler());
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
---
+++ LockerApi/MessageHandlers/APIKeyMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LockerApi.MessageHandlers
+{
+    public class APIKeyMessageHandler : DelegatingHandler
+    {
+        private const string _apiKeyToCheck = APIKey.API_KEY;
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage message, CancellationToken cancellationToken)
+        {
+            bool validKey = false;
+            IEnumerable<string> requestHeaders;
+            var checkApiKeyExist = message.Headers.TryGetValues("APIKey", out requestHeaders);
+            if (checkApiKeyExist)
+            {
+                if (requestHeaders.FirstOrDefault().Equals(_apiKeyToCheck))
+                    validKey = true;
+
+            }
+            if (!validKey)
+                return message.CreateResponse(HttpStatusCode.Forbidden, "Invalid API Key");
+
+            var response = await base.SendAsync(message, cancellationToken);
+            return response;
+        }
+    }
+}
